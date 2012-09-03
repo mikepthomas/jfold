45bed3d63817c56ff5ad78fedc934bdd81e67e61
montycrabapple1@gmail.com <montycrabapple1@gmail.com@fdc76373-fb72-6f72-1df3-f702d7f48725>
2012-09-03 22:08:50 +0000

    Initial import, SimulationInfo and SlotOptions

---
+++ trunk/src/main/java/com/googlecode/jfold/interfaces/ISlotOptions.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (C) 2012 Michael Thomas <michael4.thomas@live.uwe.ac.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.googlecode.jfold.interfaces;
+
+/**
+ *
+ * @author Michael Thomas <michael4.thomas@live.uwe.ac.uk>
+ */
+public interface ISlotOptions {
+    
+    /**
+     * Get the value of <code>client-type</code>
+     *
+     * @return the value of <code>client-type</code> from <code>slot-options</code>
+     */
+    public String getClientType();
+    
+    /**
+     * Get the value of <code>client-subtype</code>
+     *
+     * @return the value of <code>client-subtype</code> from <code>slot-options</code>
+     */
+    public String getClientSubtype();
+    
+    /**
+     * Get the value of <code>machine-id</code>
+     *
+     * @return the value of <code>machine-id</code> from <code>slot-options</code>
+     */
+    public int getMachineId();
+    
+    /**
+     * Get the value of <code>max-packet-size</code>
+     *
+     * @return the value of <code>max-packet-size</code> from <code>slot-options</code>
+     */
+    public String getMaxPacketSize();
+    
+    /**
+     * Get the value of <code>core-priority</code>
+     *
+     * @return the value of <code>core-priority</code> from <code>slot-options</code>
+     */
+    public String getCorePriority();
+    
+    /**
+     * Get the value of <code>next-unit-percentage</code>
+     *
+     * @return the value of <code>next-unit-percentage</code> from <code>slot-options</code>
+     */
+    public int getNextUnitPercentage();
+    
+    /**
+     * Get the value of <code>max-units</code>
+     *
+     * @return the value of <code>max-units</code> from <code>slot-options</code>
+     */
+    public int getMaxUnits();
+    
+    /**
+     * Get the value of <code>checkpoint</code>
+     *
+     * @return the value of <code>checkpoint</code> from <code>slot-options</code>
+     */
+    public int getCheckpoint();
+    
+    /**
+     * Get the value of <code>pause-on-start</code>
+     *
+     * @return the value of <code>pause-on-start</code> from <code>slot-options</code>
+     */
+    public boolean getPauseOnStart();
+    
+    /**
+     * Get the value of <code>gpu-vendor-id</code>
+     *
+     * @return the value of <code>client-type</code> from <code>slot-options</code>
+     */
+    public String getGpuVendorId();
+    
+    /**
+     * Get the value of <code>gpu-device-id</code>
+     *
+     * @return the value of <code>gpu-device-id</code> from <code>slot-options</code>
+     */
+    public String getGpuDeviceId();
+}
---
+++ trunk/src/test/java/com/googlecode/jfold/model/SlotOptionsTest.java
@@ -0,0 +1,201 @@
+/*
+ * Copyright (C) 2012 Michael Thomas <michael4.thomas@live.uwe.ac.uk>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.googlecode.jfold.model;
+
+import com.google.gson.Gson;
+import org.junit.AfterClass;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Michael Thomas <michael4.thomas@live.uwe.ac.uk>
+ */
+public class SlotOptionsTest {
+    
+    private static SlotOptions instance;
+    
+    public SlotOptionsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+        String json = "{\n" +
+                      "  \"client-type\": \"normal\",\n" +
+                      "  \"client-subtype\": \"SMP\",\n" +
+                      "  \"machine-id\": \"0\",\n" +
+                      "  \"max-packet-size\": \"normal\",\n" +
+                      "  \"core-priority\": \"idle\",\n" +
+                      "  \"next-unit-percentage\": \"99\",\n" +
+                      "  \"max-units\": \"0\",\n" +
+                      "  \"checkpoint\": \"15\",\n" +
+                      "  \"pause-on-start\": \"true\",\n" +
+                      "  \"gpu-vendor-id\": None,\n" +
+                      "  \"gpu-device-id\": None\n" +
+                      "}";
+        
+        Gson gson = new Gson();
+        instance = gson.fromJson(json, SlotOptions.class);
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    /**
+     * Test of getClientType method, of class SlotOptions.
+     */
+    @Test
+    public void testGetClientType() {
+        System.out.println("getClientType");
+        String expResult = "normal";
+        String result = instance.getClientType();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getClientSubtype method, of class SlotOptions.
+     */
+    @Test
+    public void testGetClientSubtype() {
+        System.out.println("getClientSubtype");
+        String expResult = "SMP";
+        String result = instance.getClientSubtype();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getMachineId method, of class SlotOptions.
+     */
+    @Test
+    public void testGetMachineId() {
+        System.out.println("getMachineId");
+        int expResult = 0;
+        int result = instance.getMachineId();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getMaxPacketSize method, of class SlotOptions.
+     */
+    @Test
+    public void testGetMaxPacketSize() {
+        System.out.println("getMaxPacketSize");
+        String expResult = "normal";
+        String result = instance.getMaxPacketSize();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getCorePriority method, of class SlotOptions.
+     */
+    @Test
+    public void testGetCorePriority() {
+        System.out.println("getCorePriority");
+        String expResult = "idle";
+        String result = instance.getCorePriority();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getNextUnitPercentage method, of class SlotOptions.
+     */
+    @Test
+    public void testGetNextUnitPercentage() {
+        System.out.println("getNextUnitPercentage");
+        int expResult = 99;
+        int result = instance.getNextUnitPercentage();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getMaxUnits method, of class SlotOptions.
+     */
+    @Test
+    public void testGetMaxUnits() {
+        System.out.println("getMaxUnits");
+        int expResult = 0;
+        int result = instance.getMaxUnits();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getCheckpoint method, of class SlotOptions.
+     */
+    @Test
+    public void testGetCheckpoint() {
+        System.out.println("getCheckpoint");
+        int expResult = 15;
+        int result = instance.getCheckpoint();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getPauseOnStart method, of class SlotOptions.
+     */
+    @Test
+    public void testGetPauseOnStart() {
+        System.out.println("getPauseOnStart");
+        boolean expResult = true;
+        boolean result = instance.getPauseOnStart();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getGpuVendorId method, of class SlotOptions.
+     */
+    @Test
+    public void testGetGpuVendorId() {
+        System.out.println("getGpuVendorId");
+        String expResult = "None";
+        String result = instance.getGpuVendorId();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of getGpuDeviceId method, of class SlotOptions.
+     */
+    @Test
+    public void testGetGpuDeviceId() {
+        System.out.println("getGpuDeviceId");
+        String expResult = "None";
+        String result = instance.getGpuDeviceId();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of toString method, of class SlotOptions.
+     */
+    @Test
+    public void testToString() {
+        System.out.println("toString");
+        String expResult = "clientType = normal" +
+                           "\nclientSubtype = SMP" +
+                           "\nmachineId = 0" +
+                           "\nmaxPacketSize = normal" +
+                           "\ncorePriority = idle" +
+                           "\nnextUnitPercentage = 99" +
+                           "\nmaxUnits = 0" +
+                           "\ncheckpoint = 15" +
+                           "\npauseOnStart = true" +
+                           "\ngpuVendorId = None" +
+                           "\ngpuDeviceId = None";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+    }
+}
