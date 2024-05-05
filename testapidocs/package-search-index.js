2f6b5df588fa8334332e28463d903146bd15ac7e
Mike Thomas <mikepthomas@outlook.com>
2024-05-05 14:32:27 +0100

    Site checkin for project jFold

--- testapidocs/package-search-index.js
+++ testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"info.mikethomas.jfold"},{"l":"info.mikethomas.jfold.options"},{"l":"info.mikethomas.jfold.simulation"},{"l":"info.mikethomas.jfold.slot"},{"l":"info.mikethomas.jfold.unit"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"info.mikethomas.jfold"},{"l":"info.mikethomas.jfold.options"},{"l":"info.mikethomas.jfold.simulation"},{"l":"info.mikethomas.jfold.slot"},{"l":"info.mikethomas.jfold.unit"}];updateSearchResults();
